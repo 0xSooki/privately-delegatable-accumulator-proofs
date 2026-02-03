ca689251a9c9e52a1801bf6c750b62c79a2058ec
Glasz Botond <glasz.botond@fornax.hu>
2026-02-03 14:14:24 +0100

    add/del with hash_to_prime

--- src/rsa_accumulator.rs
+++ src/rsa_accumulator.rs
@@ -2,6 +2,7 @@ use num_bigint::{BigInt, BigUint, RandBigInt, ToBigUint};
 use num_traits::One;
 use std::collections::HashSet;
 use glass_pumpkin::prime;
+extern crate primes;
 
 const KEY_SIZE: u64 = 512; // This key size is just for demonstration
 
@@ -40,8 +41,16 @@ impl RsaAccumulator {
         }
     }
 
-    pub fn add(&mut self, x: BigUint) {
+    pub fn add(&mut self, mut x: BigUint) {
         // Hash x to a prime before using it in the accumulator.
+        let x_str = x.to_string();
+        let vec = vec![x_str.as_str()];
+
+        let prime_u128 = primes::hash_to_prime(vec);
+        
+        let x_prime = BigUint::from(prime_u128);
+        x = x_prime;
+        print!("{}", x);
         // This ensures x is coprime to the totient, so its modular inverse exists.
         // Otherwise, removing the element may fail if the inverse does not exist.
         if self.set.contains(&x) {
@@ -55,14 +64,34 @@ impl RsaAccumulator {
         // If elements are hashed to primes during addition, the modular inverse will exist.
         // This ensures deletion works without panicking.
         // `cargo run` won't panick.
-        if !self.set.contains(&x) {}
-        else {
-            self.set.remove(&x);
+        let x_str = x.to_string();
+        let vec = vec![x_str.as_str()];
+        let prime_u128 = primes::hash_to_prime(vec);
+        let x_prime = BigUint::from(prime_u128);
 
-            let xinv = x.modinv(&self.totient).unwrap();
+        if !self.set.contains(&x_prime) {}
+        else {
+            self.set.remove(&x_prime);
+
+            let product = self.calculate_product(&self.set);
+            self.acc = self.g.modpow(&product, &self.n);
+
+            //let xinv = x.modinv(&self.totient).unwrap();
+
             
-            self.acc = self.acc.modpow(&xinv.to_biguint().unwrap(), &self.n);
+            //self.acc = self.acc.modpow(&xinv.to_biguint().unwrap(), &self.n);
         }
+    }
+
+    fn calculate_product(&self, set: &HashSet<BigUint>) -> BigUint {
+        if set.is_empty() {
+            return BigUint::one();
+        }
+        let mut product = BigUint::one();
+        for s in set {
+            product *= s;
+        }
+        product
     }
 
     pub fn mem_proof_create(acc: &RsaAccumulator, x: &BigUint) -> BigUint {
