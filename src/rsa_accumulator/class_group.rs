1c2568798031414bfa551782e36e1c9bace5142b
0xSooki <0xsooki@gmail.com>
2026-03-28 08:36:32 +0100

    tests(rsa_acc): mirror rsa group instance tests to class group

--- src/rsa_accumulator/class_group.rs
+++ src/rsa_accumulator/class_group.rs
@@ -30,16 +30,12 @@ impl RsaAccumulator<ClassGroup> {
         Self::new(group)
     }
 
-    pub fn del<T: ToString>(&mut self, element: &T) {
-        let x_str = element.to_string();
-        let x_prime = ClassGroup::hash_bytes_to_prime(x_str.as_bytes());
-        let x_exp = ClassGroupExponent(x_prime);
-
-        if self.set.remove(&x_exp) {
+    pub fn del(&mut self, element: &ClassGroupExponent) {
+        if self.set.remove(element) {
             self.acc = self
                 .set
                 .iter()
-                .filter(|&e| e != &x_exp)
+                .filter(|&e| e != element)
                 .fold(self.group.g(), |acc, x| self.group.exp(&acc, &x));
         }
     }
@@ -91,19 +87,22 @@ impl Accumulator for RsaAccumulator<ClassGroup> {
 
     fn add(&mut self, element: &Self::Element) -> <Self::Group as Group>::Exponent {
         unsafe { pari_init(100000000000, 2) };
-        if !self.set.contains(element) {
-            self.set.insert(element.clone());
-            self.acc = self.group.exp(&self.acc, element);
+        let x_str = format!("{:?}", element);
+        let x_prime = self.group.hash_to_prime(x_str.as_bytes());
+        if !self.set.contains(&x_prime) {
+            self.set.insert(x_prime.clone());
+            self.acc = self.group.exp(&self.acc, &x_prime);
         }
-        element.clone()
+        x_prime.clone()
     }
 
     fn del(&mut self, element: &Self::Element) {
         unsafe { pari_init(100000000000, 2) };
         if self.set.remove(element) {
-            let mask = self.group.exp(&self.group.g(), element);
-            let mask_inv = self.group.inv(&mask);
-            self.acc = self.group.mul(&self.acc, &mask_inv);
+            self.acc = self
+                .set
+                .iter()
+                .fold(self.group.g(), |acc, x| self.group.exp(&acc, x));
         }
     }
 
@@ -116,15 +115,13 @@ impl Accumulator for RsaAccumulator<ClassGroup> {
         element: &<Self::Group as Group>::Exponent,
     ) -> Self::MembershipProof {
         unsafe { pari_init(100000000000, 2) };
-        let x_str = format!("{:?}", element);
-        let x_prime = self.group.hash_to_prime(x_str.as_bytes());
-        if !self.set.contains(&x_prime) {
+        if !self.set.contains(&element) {
             panic!("Element not in accumulator set");
         }
         let prod = self
             .set
             .iter()
-            .filter(|s| *s != &x_prime)
+            .filter(|s| *s != element)
             .fold(ClassGroup::exp_id(), |acc, s| ClassGroup::exp_mul(&acc, s));
         self.group.exp(&self.group.g(), &prod)
     }
@@ -135,7 +132,7 @@ impl Accumulator for RsaAccumulator<ClassGroup> {
         element: &<Self::Group as Group>::Exponent,
     ) -> bool {
         unsafe { pari_init(100000000000, 2) };
-        self.mem_ver(proof, &element)
+        self.group.exp(proof, element) == self.acc
     }
 
     fn non_mem_proof_create(&self, element: &Self::Element) -> Self::NonMembershipProof {
@@ -248,48 +245,160 @@ impl PrivatelyDelegatableAccumulator for RsaAccumulator<ClassGroup> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::traits::Accumulator;
-    use crate::traits::Group;
-    use crate::traits::PrivatelyDelegatableAccumulator;
-    use crate::RsaAccumulator;
-    fn setup_class_acc() -> RsaAccumulator<ClassGroup> {
-        RsaAccumulator::new(ClassGroup::setup())
+    use crate::traits::{Accumulator, Group, PrivatelyDelegatableAccumulator};
+
+    fn hash_input(acc: &RsaAccumulator<ClassGroup>, value: u32) -> ClassGroupExponent {
+        acc.group.hash_to_prime(value.to_string().as_bytes())
     }
 
     #[test]
     fn test_acc_add_del_no_change() {
-        let mut acc = setup_class_acc();
+        let mut acc = RsaAccumulator::<ClassGroup>::setup();
+        let initial_acc = acc.acc.clone();
+        let element = "test_element";
 
-        let initial_acc = acc.value().clone();
-        acc.add(&ClassGroupExponent(BigInt::from(42)));
-        acc.del(&ClassGroupExponent(BigInt::from(42)));
+        let ep = acc.add(&element);
+        acc.del(&ep);
 
-        assert_eq!(acc.value(), &initial_acc);
+        assert_eq!(
+            acc.acc, initial_acc,
+            "Accumulator value should be unchanged after add and remove of the same element"
+        );
     }
 
     #[test]
-    fn test_mem_proof_verifies() {
-        let mut acc = setup_class_acc();
+    fn test_gen_mem_proof() {
+        let mut acc = RsaAccumulator::<ClassGroup>::setup();
+        let element = 7usize;
+        let ep = acc.add(&element);
 
-        let m1 = ClassGroupExponent(BigInt::from(7));
-        acc.add(&m1);
-        acc.add(&ClassGroupExponent(BigInt::from(11)));
-        acc.add(&ClassGroupExponent(BigInt::from(13)));
+        for i in 2..5 {
+            acc.add(&i);
+        }
 
-        let proof = acc.mem_proof_create(&m1);
-        assert!(acc.mem_ver(&proof, &m1));
+        let proof = acc.mem_proof_create(&ep);
+
+        assert!(acc.mem_ver(&proof, &ep));
     }
 
     #[test]
-    fn test_mem_proof_not_verifies() {
-        let mut acc = setup_class_acc();
+    fn test_non_mem_proof() {
+        let mut acc = RsaAccumulator::<ClassGroup>::setup();
 
-        acc.add(&ClassGroupExponent(BigInt::from(7)));
-        acc.add(&ClassGroupExponent(BigInt::from(11)));
-        acc.add(&ClassGroupExponent(BigInt::from(13)));
-        let non_member = ClassGroupExponent(BigInt::from(17));
+        acc.add(&2u32);
+        acc.add(&3u32);
+        acc.add(&7u32);
 
-        let proof = acc.mem_proof_create(&non_member);
-        assert!(!acc.mem_ver(&proof, &non_member));
+        let non_member = hash_input(&acc, 5u32);
+
+        let proof = acc.non_mem_proof_create(&non_member);
+        assert!(
+            acc.non_mem_ver(&proof, &non_member),
+            "Non-membership proof should verify"
+        );
+    }
+
+    #[test]
+    fn test_blind_unblind_mem() {
+        let mut acc = RsaAccumulator::<ClassGroup>::setup();
+
+        let element = 7usize;
+        let ep = acc.add(&element);
+
+        for i in 2..5 {
+            acc.add(&i);
+        }
+
+        let proof = acc.mem_proof_create(&ep);
+
+        let blinded_proof = acc.blind_mem_proof(&proof);
+
+        assert!(
+            blinded_proof.0 != proof,
+            "Proof is not blinded successfully"
+        );
+
+        let unblinded_proof = acc.unblind_mem_proof(&blinded_proof.0, &blinded_proof.1);
+        assert!(
+            unblinded_proof == proof,
+            "Proof is not unblinded successfully"
+        );
+    }
+
+    #[test]
+    fn test_blind_mem_proof_upd_ver() {
+        let mut acc = RsaAccumulator::<ClassGroup>::setup();
+
+        let ep = acc.add(&200003u32);
+
+        let acct = acc.acc.clone();
+
+        let proof = acc.mem_proof_create(&ep);
+
+        let elements_in = vec![65537u32, 100003u32, 104729u32, 1299709u32, 15485863u32];
+
+        let elements_out = vec![];
+        let elements_in = elements_in.iter().map(|e| acc.add(e)).collect::<Vec<_>>();
+
+        let blinded_proof = acc.blind_mem_proof(&proof);
+
+        let upd_blind_proof =
+            acc.blind_mem_proof_upd(elements_in, elements_out, &acct, &blinded_proof.0);
+
+        assert!(acc.ver_blind_mem_proof_upd(
+            &acct,
+            &blinded_proof.0,
+            &upd_blind_proof.0,
+            &upd_blind_proof.1
+        ));
+    }
+
+    #[test]
+    fn test_blind_unblind_non_mem() {
+        let mut acc = RsaAccumulator::<ClassGroup>::setup();
+
+        for i in 2..5 {
+            acc.add(&i);
+        }
+
+        let non_member = hash_input(&acc, 7u32);
+
+        let blinded_proof = acc.blind_non_mem_proof(&non_member);
+
+        for i in 10..12 {
+            acc.add(&i);
+        }
+
+        let upd_blind_non_mem_proof = acc.blind_non_mem_proof_upd(&blinded_proof);
+
+        let unblinded_proof = acc.unblind_non_mem_proof(&blinded_proof, &upd_blind_non_mem_proof);
+        assert!(
+            acc.non_mem_ver(&unblinded_proof, &non_member),
+            "Non-membership proof should verify after unblinding"
+        );
+    }
+
+    #[test]
+    fn test_blind_non_mem_proof_upd_ver() {
+        let mut acc = RsaAccumulator::<ClassGroup>::setup();
+
+        let non_member = hash_input(&acc, 200003u32);
+
+        let blinded_proof = acc.blind_non_mem_proof(&non_member);
+
+        let elements_in = vec![65537u32, 100003u32, 104729u32, 1299709u32, 15485863u32];
+
+        for elem in &elements_in {
+            acc.add(elem);
+        }
+
+        let acctprime = acc.acc.clone();
+
+        let upd_blind_proof = acc.blind_non_mem_proof_upd(&blinded_proof);
+
+        assert!(
+            acc.ver_blind_non_mem_proof_upd(&acctprime, &blinded_proof, &upd_blind_proof),
+            "Couldnt verify"
+        );
     }
 }
